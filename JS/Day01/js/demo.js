6bc5359c73b2a3e57942ce4a8cdf295c1e83cc10
sara <sara.atef001@gmail.com>
2024-12-18 11:53:04 +0200

    day01

---
+++ JS/Day01/js/demo.js
@@ -0,0 +1,142 @@
+// var fname = "a"; // string
+// var num = 10; // number
+// var flag = true; //boolean
+// var num2 = 10.23; // number
+// var trackName;
+
+// console.log(typeof trackName);
+
+// console.log(typeof fname);
+// console.log(typeof num); // 10
+// console.log(typeof flag);
+// console.log(typeof num2);
+// var fname = "mona";
+// fname = "ali";
+// fname = 10;
+// fname = true;
+// console.log(typeof fname);
+
+// var fname = "mona";
+// var fname = "ahmed";
+// console.log(fname);
+// var fname;
+// console.log(typeof fname);
+// fname = 10;
+// console.log(typeof fname);
+
+// console.log(fname);
+
+// var student = null; // js student = {} -- object
+// console.log(typeof student);
+
+// var x = 10; //number
+
+// var fname = String(10);
+
+// console.log(typeof fname);
+
+// var num = 10;
+
+// var num = Number("mona10");
+// console.log(num); //NaN -- not a number
+// console.log(typeof num); //
+
+// var flag = Boolean("mona  l,fvkmd ");
+
+// console.log(flag);
+// camel case
+// var first = "";
+// var firstName = "ahmed ali";
+// var firstSecondThird = "ahmed ali";
+// var firstSecondThirdFouthFith = "ali";
+// var FNAME = "ahmed";
+// console.log(fname);
+// console.log(FNAME);
+
+// var x = "mona"; //string
+// var y = 10; // number String(10) == 10 as string
+// console.log(y + x);
+// console.log(typeof y);
+
+// var x = "10"; // Number -- NaN
+
+// var y = 10;
+
+// console.log(x - y);
+/**--------------- Comparasion Operator
+ * == [==] --> values only
+ * == [===] --> values And Datatypes
+ * -------------- */
+// var x = 0; //
+// var y = false; // 20 -- number Number(true) 1
+// console.log(x == y);
+/**
+ *&&
+ *---------
+    false && false --> false
+    false && true --> false
+    true && false --> false
+    true && true --> true
+    ------------------------------
+ * ||
+ *---------
+    false || false --> false
+    false || true --> true
+    true || false --> true
+    true && true --> true
+
+    ----------------
+    Not
+    -- true --> false
+    -- false --> true
+    -------short circut--------------------
+
+    mona && ali
+    true && true
+    0 && "OS"
+    0
+
+
+    undefined || "front"
+    5 || 0
+ */
+/**----------------------- */
+// for (var i = 0; i < 5; i++) {
+// 	console.log(i);
+// }
+
+// var i = 0;
+// while ( i < 5 ) {
+//     console.log( i )
+//     i++
+// }
+// var fname = prompt("Enter Your Name", "Default Name");
+// console.log(fname);
+
+// var num1 = Number(prompt("Enter Number1", 10));
+// var num2 = Number(prompt("Enter Number2", 20));
+// console.log(num1 + num2);
+// var i = 4;
+// document.writeln("Hello Open source");
+// document.writeln("<h1>Welcome at ITI</h1>");
+// document.writeln("<h" + i + ">Welcome at ITI</h" + i + ">");
+// document.writeln("<p class='jsClass'>Welcome at ITI</p>");
+// document.writeln("<p style='color:green;font-size:30px'>Welcome at ITI</p>");
+// document.writeln("<img src='1.jpg'/>");
+// var lname = "ahmed";
+
+// console.log("<h1>" + lname + "</h1>");
+// alert("you deleted this item");
+
+// var result = confirm("are you sure you want to buy this course");
+
+// // (condition)?// true://false
+
+// result
+// 	? document.writeln("Course added to your list")
+// 	: document.writeln("Operation cancelled");
+
+// var x = 0;
+// var y = false;
+
+// console.log(x == y);
