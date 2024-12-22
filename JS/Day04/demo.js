a467504924bbc564224b9c927a7e15f3fbdaefd2
sara <sara.atef001@gmail.com>
2024-12-22 15:48:51 +0200

    day04

---
+++ JS/Day04/demo.js
@@ -0,0 +1,67 @@
+// console.log(document.links);
+// console.log(document.images[0]);
+// console.log(document.images[1]);
+
+// console.log(document.body); // select body
+// var element = document.getElementById("header");
+// console.log(element.firstChild);
+// console.log(element.firstElementChild);
+/**----- How to Fire an Event in JS  ------ */
+// 1- add Attribute on the element itself
+
+// function show() {
+// 	// logic
+// 	console.log("Btn Clicked");
+// }
+// function sayHello() {
+// 	// logic
+// 	console.log("hello");
+// }
+
+// var element = document.getElementsByClassName("info")[1];
+// console.log(element);
+// function changeContent() {
+// 	// get
+// 	// console.log(h1.innerText);
+// 	// set -- override
+// 	element.innerHTML = "<h2>Hello OS</h1>";
+// 	//element.innerHTML = "Hello OS";
+// }
+
+// var input1 = document.querySelector(`input`);
+// var output = document.getElementById("output");
+
+// function getData() {
+// 	output.innerHTML = input1.value;
+// 	// //get
+// 	// // console.log(input1.value);
+// 	// input1.value = "OS";
+// }
+
+// var inputs = document.getElementsByName("choose");
+
+// function execute() {
+// 	for (var i = 0; i < inputs.length; i++) {
+// 		if (inputs[i].checked) {
+// 			console.log(inputs[i].value);
+// 		}
+// 	}
+// }
+
+// var p = document.getElementById("first");
+
+// function changeStyle() {
+// 	p.classList.toggle("jsClass");
+// 	// console.log(p.classList);
+// }
+// function changeStyle() {
+// 	p.className += " jsClass";
+// 	// console.log(p.className);//
+// }
+//function changeStyle() {
+// p.style.backgroundColor = "#c97373";
+// p.style.color = "#fff";
+// p.style.padding = "2rem";
+// p.style.borderRadius = "1rem";
+//p.style.cssText = "background-color:red;padding:2rem;color:#fff";
+//}
